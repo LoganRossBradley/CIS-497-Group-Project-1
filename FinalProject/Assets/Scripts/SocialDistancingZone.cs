d7816db348a343e50ab9de1210b89bb9efcde6e6
CaptainJ61 <difigliojimmy@gmail.com>
2020-09-21 16:59:00 -0500

    MinorAdditions
    Added social distancing script to obstacles to delete player when player enters a certain distance

--- FinalProject/Assets/Scripts/SocialDistancingZone.cs
+++ FinalProject/Assets/Scripts/SocialDistancingZone.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class SocialDistancingZone : MonoBehaviour
 {
 
-    public GameObject player;
-
-    private Vector3 offset = new Vector3(0, 0, 0);
-
-
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
     }
 
 }
