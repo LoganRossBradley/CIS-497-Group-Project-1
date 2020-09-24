5c8d3e3ef0e5fe095813936ba9f1e28faf6dd2d7
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2020-09-23 19:32:07 -0500

    Added Timer and Compass
    Added Timer, Timer UI, and compass.
    -Ben Schuster

---
+++ FinalProject/Assets/Scripts/Compass.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Benjamin Schuster
+ * Project 1
+ * Rotates the compass so it will point towards the goal 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public GameObject goal;
+    public GameObject arrow;
+    public float viewDistance;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set the initial distance between player and goal
+        distance = Vector3.Distance(transform.position, goal.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate compass to point towards the goal
+        transform.LookAt(goal.transform);
+
+        //Have the arrow disappear if the player is close enough to goal, reappear if strays too far from goal
+        distance = Vector3.Distance(transform.position, goal.transform.position);
+        if (distance <= viewDistance)
+        {
+            arrow.GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+            arrow.GetComponent<MeshRenderer>().enabled = true;
+        }
+
+    }
+}
---
+++ FinalProject/Assets/Scripts/Countdown.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Benjamin Schuster
+ * Project 1
+ * Countdown timer for the game, update UI timer
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Countdown : MonoBehaviour
+{
+    public float timeLeft;
+    public Text timerRef;
+    //this can be changed to a reference once we have a proper system controller
+    public bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Either countdown or set gamestate to gameover if out of time
+        if(timeLeft > 0 && !gameOver)
+        {
+            timeLeft -= Time.deltaTime;
+            timerRef.text = "Time Remaining:\n" + timeLeft.ToString("F0");
+        }
+        else if(timeLeft <= 0 && !gameOver)
+        {
+            Debug.Log("Out of Time! Game Over!");
+            gameOver = true;
+        }
+    }
+
+    //Powerups can call this so time can be added 
+    void addTime()
+    {
+        timeLeft += 10.0f;
+    }
+}
